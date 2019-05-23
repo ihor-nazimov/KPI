84edbe49bf71b64cb036a65dfd4449093338707b
ihor-nazimov <ihor.nazimov@gmail.com>
2019-05-24 00:29:38 +0300

    lab3 final

--- OOP/1k_2s/lab3.cpp
+++ OOP/1k_2s/lab3.cpp
@@ -7,6 +7,20 @@
 4. *** на размер объектов конечного класса 
 */
 
+//non virtual
+// sizeof(Date)12
+// sizeof(Person)20
+// sizeof(Position)24
+// sizeof(Employee)56
+// sizeof(Sector)72
+
+//virtual show() and display()
+// sizeof(Date)16
+// sizeof(Person)24
+// sizeof(Position)24
+// sizeof(Employee)64
+// sizeof(Sector)80
+
 #include <cstdlib>
 //#include <stdio.h>
 #include <string>
@@ -22,6 +36,7 @@ class Date {
         unsigned year = 0; 
         unsigned month = 0;
         unsigned day = 0;
+        const Date& _copy(const Date& src) { return *this = src; }
     public:
         Date() {}
         Date(unsigned year, unsigned month, unsigned day): year(year), month(month), day(day) {}
@@ -34,9 +49,16 @@ class Date {
 
 class Position {
     private:
-        char* _title = 0;
-        double _salary = 0;
         bool _vacant = false;
+        double _salary = 0;
+        char* _title = 0;
+    protected:
+        const Position& _copy(const Position& src) {
+            *this = src;
+            _title = 0;
+            title(const_cast<Position&>(src).title());
+            return *this;
+        } 
     public:
         Position() {}
         Position(const char* title, double salary=0, bool vacant=false): 
@@ -44,7 +66,7 @@ class Position {
             Position::title(title);
         }
         Position(const Position& pos): _salary(pos._salary), _vacant(pos._vacant) {
-            Position::title(pos._title);
+            _copy(pos);
         }
         ~Position() {
             if (_title) delete[] _title;
@@ -59,7 +81,7 @@ class Position {
             return _title;
         }
         virtual void display() {
-            printf("Class Position(@%p): %s, salary=%10.2lg, %s\n", this, _title, _salary, _vacant ? "vacant" : "occupied");
+            printf("Class Position(@%p): %s, salary=%10.2lf, %s\n", this, _title, _salary, _vacant ? "vacant" : "occupied");
         }
 };
 
@@ -67,6 +89,16 @@ class Position {
 class Person: public Date {
         char* _name = 0;
         char* _surname = 0;
+    protected:
+        const Person& _copy(const Person& src) {
+            *this = src;
+            _name = 0;
+            _surname = 0;
+            surname(const_cast<Person&>(src).surname());
+            name(const_cast<Person&>(src).name());
+            Date::_copy((Date)*this);
+            return *this; 
+        }
     public:
         Person() {}
         Person(const char* nm, const char* surnm, unsigned year, unsigned month, unsigned day) {
@@ -77,8 +109,7 @@ class Person: public Date {
             surname(surnm);
         }
         Person(const Person& pers) {
-            name(pers._name);
-            surname(pers._surname);
+            _copy(pers);
         }
         ~Person() {
             if (_name) delete[] _name;
@@ -110,20 +141,21 @@ class Person: public Date {
 
 class Employee: public Person, public Position { //public Date when was employed - inheritance is bad idea. Use incapsulation
         Date employed;
+    protected:
+        const Employee& _copy(const Employee& src) {
+            *this = src;
+            Person::_copy((Person) src);
+            Position::_copy((Position) src);
+        }
     public:
         Employee() {}
         Employee(Person& pers, Position& pos, Date& empld) {
-            Person* tmppers = (Person*) this;
-            *tmppers = pers;
-            Position* tmppos = (Position*) this;
-            *tmppos = pos;
-            employed = empld;
+            Person::_copy((Person) pers);
+            Position::_copy((Position) pos);
+            employed = empld; //copy Date fields
         }
         Employee(const Employee &emp): employed(emp.employed) {
-            Person* tmppers = (Person*) this;
-            *tmppers = (Person) emp;
-            Position* tmppos = (Position*) this;
-            *tmppos = (Position) emp;
+            _copy(emp);        
         }
         ~Employee() {
         }
@@ -134,24 +166,28 @@ class Employee: public Person, public Position { //public Date when was employed
         }
 };
 
-class Sector: public Employee { //Person - head of sector
+class Sector: public Employee { //Person is head of sector
         char* _sectorname = 0;
         //head of sector placed in base class Employee
     public:
-        Employee _elist[MAX_EMPLOYEES-1];
+        Employee* _elist[MAX_EMPLOYEES];
         Sector() {}
         Sector(const char* sectnm, Employee& head) {
             sectorname(sectnm);
-            Employee* tmphead = (Employee*) this;
-            *tmphead = head;
+            Employee::_copy(head);
+            for (int i=0; i<MAX_EMPLOYEES-1; i++) 
+                _elist[i] = 0;
+
         }
         Sector(const Sector& sect) {
             sectorname(sect._sectorname);
-            Employee* tmphead = (Employee*) this;
-            *tmphead = (Employee) sect;
-            for (int i=0; i<MAX_EMPLOYEES-1; i++) _elist[i] = sect._elist[i];
+            Employee::_copy((Employee) sect);
+            for (int i=0; i<MAX_EMPLOYEES-1; i++) 
+                _elist[i] = new Employee(*sect._elist[i]);
         }
         ~Sector() {
+            for (int i=0; i<MAX_EMPLOYEES-1; i++) 
+                if (_elist[i]) delete _elist[i];
             if (Sector::_sectorname) delete[] Sector::_sectorname;
         }
         const char* sectorname(const char* sectorname=0) {
@@ -165,104 +201,64 @@ class Sector: public Employee { //Person - head of sector
         void display() {
             printf("Class Sector(@%p):\n", this);
             printf("\tHead of sector: "); Employee::show();
-            printf("\tEmployees:\n");
+            printf("\tEmployee list:\n");
             for (int i = 0; i < MAX_EMPLOYEES; i++) {
-                printf("\t");
-                _elist[i].show();
+                printf("\t#%i\n", i);
+                _elist[i]->show();
             }
         }
 };
 
 int main(int argc, char** argv) {
-    Date dt(2019, 5, 8);
-    Date* dt2 = new Date(dt);
-    std::cout << "sizeof(Date)" << sizeof(dt) << std::endl;
-    std::cout << "sizeof(Date*)" << sizeof(dt2) << std::endl;
-    std::cout <<  "sizeof(Date)" << sizeof(*dt2) << std::endl;
-
-
-    Position pos = Position("CEO", 1000, true);
-    std::cout << "sizeof(Position)" << sizeof(pos) << std::endl;
-    //std::cout << "sizeof(Position*)" << sizeof(pos) << std::endl;
-
-    Person pers;// = Person(1976, 6, 28);
-    std::cout << "sizeof(Person)" << sizeof(pers) << std::endl;
-    // delete dt2;
-
-    //Mr. Sherlock Holmes
-    //Private detectiive
-    Position* holmes = new Position("detective", 2000, false);
-    std::cout << "sizeof(Position*)" << sizeof(holmes) << std::endl;
-    std::cout << "sizeof(Position)" << sizeof(*holmes) << std::endl;
-    cout << holmes->title() << endl;
-    holmes->title("consulting detective");
-    cout << holmes->title() << endl;
-    holmes->display();
-
-    Person* pers1 = new Person();
-    Person* sherlock = new Person("Sherlock", "Holmes", 1850, 1, 1);
-    Person* pers3 = new Person(*sherlock);
-    std::cout << "sizeof(Person*)" << sizeof(sherlock) << std::endl;
-    std::cout << "sizeof(Person)" << sizeof(*sherlock) << std::endl;
-    pers3->show();
-    cout << sherlock->name() << endl;
-    cout << sherlock->name("Mr.Sherlock") << endl;
-    cout << sherlock->surname() << endl << sherlock->surname("HOLMES") << endl;
-    sherlock->show();
-    Date* bday = (Date*) sherlock;
-    bday->show();
-    std::cout << "sizeof(Date/Person*)" << sizeof(bday) << std::endl;
-    std::cout << "sizeof(Date/Person)" << sizeof(*bday) << std::endl;
 
     std::cout << "sizeof(Date)" << sizeof(Date) << std::endl;
     std::cout << "sizeof(Person)" << sizeof(Person) << std::endl;
     std::cout << "sizeof(Position)" << sizeof(Position) << std::endl;
     std::cout << "sizeof(Employee)" << sizeof(Employee) << std::endl;
-    std::cout << "sizeof(Sector)" << sizeof(Sector) << std::endl;
+    std::cout << "sizeof(Sector)" << sizeof(Sector) << std::endl << std::endl;
 
     //head of sector
     Employee* head = new Employee(
             *new Person("Artur", "Conan Doyle", 1859, 5, 22), 
             *new Position("author", 1000000, false), 
             *new Date(1891, 01, 01)); //employed
+    cout << "Employee head is:\n";
     head->show();
+    cout << endl;
 
     Sector* bs221b = new Sector("221B Baker Street", *head);
-    bs221b->_elist[0] = Employee(
+    bs221b->_elist[0] = new Employee(
             *new Person("Sherlock", "Holmes", 1850, 1, 1), 
             *new Position("consulting detective", 100000, false), 
             *new Date(1891, 01, 02)); //employed
-    bs221b->_elist[1] = Employee(
+    bs221b->_elist[1] = new Employee(
             *new Person("John", "Watson", 1850, 1, 2), 
             *new Position("assistant", 100000, false), 
             *new Date(1891, 01, 02)); //employed
-    bs221b->_elist[2] = Employee(
+    bs221b->_elist[2] = new Employee(
             *new Person("Martha", "Hudson", 1850, 1, 3), 
             *new Position("landlady", 100000, false), 
             *new Date(1891, 01, 03)); //employed
 
+    cout << "Sector is:\n";
     bs221b->display();
-    delete head;
-
-
+    cout << endl;
     getchar();
 
-    // //Dr. John Watson  
-    // //detective's assistant
-    // Position* pos2 = new Position("assistant", 1000, false);
-    // //landlady 
-    // //Mrs. Martha Louise Hudson (née Sissons)
-    // //221C Baker Street
-    // Position* pos3 = new Position("landlady", 1000, false);
-    // //Mycroft 
-    // Position* pos4 = new Position("muse", 1000, false);
-    // //Jim Moriarty
-    // //Dr.
-    // Position* pos5 = new Position("consulting criminal", 20000, false);
-    
-    
+    Date* dt = bs221b;
+    cout << "Date::show() "; dt->show(); cout << endl; 
+    Position* pos = bs221b;
+    cout << "Position::display() "; pos->display(); cout << endl; 
+    Person* pers = bs221b;
+    cout << "Person::show() "; pers->show(); cout << endl; 
+    Employee* empl = bs221b;
+    cout << "Employee::show() "; empl->show(); cout << endl; 
+    cout << "Employee::display() "; empl->display(); cout << endl; 
 
-    // delete holmes;
+    delete bs221b;
+    delete head;
     
+    getchar();
+
 
 }
